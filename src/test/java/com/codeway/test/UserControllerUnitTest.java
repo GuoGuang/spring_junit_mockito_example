dd470eaff635deb9d4bad69dbf8baf5f9642970b
guoguang0536@gmail.com <guoguang0536@gmail.com>
2020-06-11 21:03:26 +0800

    :tada: 初始化提交

---
+++ src/test/java/com/codeway/test/UserControllerUnitTest.java
@@ -0,0 +1,226 @@
+package com.codeway.test;
+
+import com.codeway.config.WebConfig;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.codeway.controller.UserController;
+import com.codeway.filter.CORSFilter;
+import com.codeway.model.User;
+import com.codeway.service.UserService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.*;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import java.util.Arrays;
+import java.util.List;
+import static org.mockito.Mockito.*;
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {WebConfig.class})
+public class UserControllerUnitTest {
+
+    private MockMvc mockMvc;
+
+	/**
+	 * 被测类中用的类使用@Mock模拟
+	 */
+	@Mock
+    private UserService userService;
+
+	/**
+	 * 模拟被测类
+	 */
+	@InjectMocks
+    private UserController userController;
+
+    @Before
+    public void init(){
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(userController)
+                .addFilters(new CORSFilter())
+                .build();
+    }
+
+
+    @Test
+    public void test_get_all_success() throws Exception {
+        List<User> users = Arrays.asList(
+                new User(1, "Foo"),
+                new User(2, "Bar"));
+
+        // 执行userService.getAll()时，返回users，这里是为了模拟/user接口的userService调用getAll返回的数据
+        when(userService.getAll()).thenReturn(users);
+
+        mockMvc.perform(get("/user"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[0].username", is("Foo")))
+                .andExpect(jsonPath("$[1].id", is(2)))
+                .andExpect(jsonPath("$[1].username", is("Bar")));
+
+        // 验证调用次数
+        verify(userService, times(1)).getAll();
+        verifyNoMoreInteractions(userService);
+    }
+
+
+    @Test
+    public void test_get_by_id_success() throws Exception {
+        User user = new User(1, "巧克力酱");
+
+        when(userService.findById(1)).thenReturn(user);
+
+        mockMvc.perform(get("/user/{id}", 1))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.username", is("巧克力酱")));
+
+        verify(userService, times(1)).findById(1);
+        verifyNoMoreInteractions(userService);
+    }
+
+    @Test
+    public void test_get_by_id_fail_404_not_found() throws Exception {
+        when(userService.findById(1)).thenReturn(null);
+
+        mockMvc.perform(get("/user/{id}", 1))
+                .andExpect(status().isNotFound());
+
+        verify(userService, times(1)).findById(1);
+        verifyNoMoreInteractions(userService);
+    }
+
+
+    @Test
+    public void test_create_user_success() throws Exception {
+        User user = new User("芝士");
+
+        when(userService.exists(user)).thenReturn(false);
+        doNothing().when(userService).create(user);
+
+        mockMvc.perform(
+                post("/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(user)))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("location", containsString("/user/0")));
+
+        verify(userService, times(1)).exists(user);
+        verify(userService, times(1)).create(user);
+        verifyNoMoreInteractions(userService);
+    }
+
+    @Test
+    public void test_create_user_fail_404_not_found() throws Exception {
+        User user = new User("username exists");
+
+        when(userService.exists(user)).thenReturn(true);
+
+        mockMvc.perform(
+                post("/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(user)))
+                .andExpect(status().isConflict());
+
+        verify(userService, times(1)).exists(user);
+        verifyNoMoreInteractions(userService);
+    }
+
+
+    @Test
+    public void test_update_user_success() throws Exception {
+        User user = new User(1, "芝士");
+
+        when(userService.findById(user.getId())).thenReturn(user);
+        doNothing().when(userService).update(user);
+
+        mockMvc.perform(
+                put("/user/{id}", user.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(user)))
+                .andExpect(status().isOk());
+
+        verify(userService, times(1)).findById(user.getId());
+        verify(userService, times(1)).update(user);
+        verifyNoMoreInteractions(userService);
+    }
+
+    @Test
+    public void test_update_user_fail_404_not_found() throws Exception {
+        User user = new User(999, "user not found");
+
+        when(userService.findById(user.getId())).thenReturn(null);
+
+        mockMvc.perform(
+                put("/user/{id}", user.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(user)))
+                .andExpect(status().isNotFound());
+
+        verify(userService, times(1)).findById(user.getId());
+        verifyNoMoreInteractions(userService);
+    }
+
+    @Test
+    public void test_delete_user_success() throws Exception {
+        User user = new User(1, "芝士");
+
+        when(userService.findById(user.getId())).thenReturn(user);
+        doNothing().when(userService).delete(user.getId());
+
+        mockMvc.perform(
+                delete("/user/{id}", user.getId()))
+                .andExpect(status().isOk());
+
+        verify(userService, times(1)).findById(user.getId());
+        verify(userService, times(1)).delete(user.getId());
+        verifyNoMoreInteractions(userService);
+    }
+
+    @Test
+    public void test_delete_user_fail_404_not_found() throws Exception {
+        User user = new User(999, "user not found");
+
+        when(userService.findById(user.getId())).thenReturn(null);
+
+        mockMvc.perform(
+                delete("/user/{id}", user.getId()))
+                .andExpect(status().isNotFound());
+
+        verify(userService, times(1)).findById(user.getId());
+        verifyNoMoreInteractions(userService);
+    }
+
+    @Test
+    public void test_cors_headers() throws Exception {
+        mockMvc.perform(get("/user"))
+                .andExpect(header().string("Access-Control-Allow-Origin", "*"))
+                .andExpect(header().string("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE"))
+                .andExpect(header().string("Access-Control-Allow-Headers", "*"))
+                .andExpect(header().string("Access-Control-Max-Age", "3600"));
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            return mapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
