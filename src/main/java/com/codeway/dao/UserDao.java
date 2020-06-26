68832159f2d925d1d4c55c392115e914a677fd75
guoguang0536@gmail.com <guoguang0536@gmail.com>
2020-06-26 15:08:01 +0800

    :zap: Improving performance.

--- src/main/java/com/codeway/dao/UserDao.java
+++ src/main/java/com/codeway/dao/UserDao.java
@@ -3,12 +3,8 @@ package com.codeway.dao;
 import com.codeway.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Repository
 public interface UserDao extends JpaRepository<User, Integer> {
